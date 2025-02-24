d3cb6becfc385f8c4e761e86bbe2064eff3709bf
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2025-02-24 12:23:25 +0530

    Add files via upload

--- Day2_Variables&datatype/index.py
+++ Day2_Variables&datatype/index.py
@@ -1,1 +1,51 @@
+# Operators
+# Arithematic Operators
+a = 3
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) # Division
+print(a % b) # Remainder
+print(a ** b)
+print(a)
 
+
+
+
+# Data Types
+ #age = 20
+#price = 25.99
+ #name ="Vagisha Yadav"
+ #isMarried = False
+ #hobbies = None
+
+# print(type(age))
+# print(type(price))
+# print(type(name))
+# print(type(isMarried))
+# print(type(hobbies))
+
+
+
+
+# name = "Vagisha Yadav"
+# age = 20
+# price = 25.99
+# lastName = "Vagisha"
+
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# Multi Line comment
+"""
+print(type(name))
+print(type(age))
+print(type(price))
+"""
+
+# print("My name is :- " , name,  "My age is:- ", age)
+# print("My age is:- ", age)
